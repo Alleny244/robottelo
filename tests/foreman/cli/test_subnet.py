59b6a523ab845e5253c00ec311363cf711336892
Alleny244 <alleny244@gmail.com>
2026-04-10 15:42:59 +0530

    feat: Adds automation test for subnet set-parameter option creation with multiple values

--- tests/foreman/cli/test_subnet.py
+++ tests/foreman/cli/test_subnet.py
@@ -261,8 +261,7 @@ def test_positive_set_parameter_option_presence(module_target_sat):
     )
 
 
-@pytest.mark.stubbed
-def test_positive_create_with_parameter_and_multiple_values():
+def test_positive_create_with_parameter_and_multiple_values(module_target_sat):
     """Subnet parameters can be created with multiple values
 
     :id: 1e9ef184-bdf9-4eba-8055-f55b0dd9d6a0
@@ -278,6 +277,20 @@ def test_positive_create_with_parameter_and_multiple_values():
 
     :BZ: 1426612
     """
+    subnet = module_target_sat.cli_factory.make_subnet()
+    param_name = gen_string('alphanumeric', 10)
+    value_parts = [gen_string('alpha', 8) for _ in range(3)]
+    comma_value = ','.join(value_parts)
+    module_target_sat.cli.Subnet.set_parameter(
+        {'subnet-id': subnet['id'], 'name': param_name, 'value': comma_value}
+    )
+    subnet_info = module_target_sat.cli.Subnet.info({'id': subnet['id']}, output_format='json')
+    params = subnet_info['parameters']
+    param = next(parameter for parameter in params if param_name in parameter['name'])
+    stored = param['value']
+    stored_str = str(stored).strip()
+    assert comma_value in stored_str or stored_str == comma_value
+    module_target_sat.cli.Subnet.delete({'id': subnet['id']})
 
 
 @pytest.mark.stubbed
